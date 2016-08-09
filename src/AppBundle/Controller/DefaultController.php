da153b740fcd389c8584a21688b9919728c68b79
Marko Pađen <markopaden3@gmail.com>
2016-08-09 15:16:35 +0200

    twig update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+
+        //$em = $this->getDoctrine()->getRepository('AppBundle:Article')->findAll();
+        //$title = $em->getTitle();
+
+        return $this->render('default/public.html.twig');
     }
 }
