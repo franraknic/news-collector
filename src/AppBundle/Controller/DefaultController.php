01b6d987ee01f7634d69046e5ecf773ebae9d309
Marko Pađen <markopaden3@gmail.com>
2016-08-11 13:02:04 +0200

    -*-

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,38 +5,59 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template("public.html.twig")
      */
     public function indexAction(Request $request)
     {
         $rep = $this->getDoctrine()->getRepository('AppBundle:Article');
-        $articles = $rep->findAll();
+        $articles = $rep->findBy(['visible' => true]);
 
-        return $this->render('default/public.html.twig', array('articles' => $articles));
+        //neznam kako drugacije provjeriti je li kategorija vidljiva
+        foreach ($articles as $article) {
+            foreach ($article->getCategories() as $category) {
+                if ($category->getVisible() == false) {
+                    if (($key = array_search($article, $articles)) !== false) {
+                        unset($articles[$key]);
+                    }
+                }
+            }
+        }
+
+        return array('articles' => $articles);
     }
 
     /**
-     * @Route("/cat/{category}", name="category")
+     * @Route("/cat/{category_id}", name="category")
+     * @Template("public.html.twig")
      */
-    public function categoryAction(Request $request, $category)
+    public function categoryAction(Request $request, $category_id)
     {
         $rep = $this->getDoctrine()->getRepository('AppBundle:Category');
 
-        $cat = $rep->findOneBy(array('name' => $category));
+        $cat = $rep->findOneBy(array('id' => $category_id, 'visible' => true));
 
-        return $this->render('default/public.html.twig', array('articles' => $cat->getArticles()));
+        if ($cat != null) {
+            return array('articles' => $cat->getArticles());
+        } else {
+            return new Response("Nažalost, kategorija privremeno nije dostupna!");
+        }
     }
+
     /**
      * @Route("/show/{id}", name="show_article")
+     * @Template("show_article.html.twig")
      */
     public function showAction(Request $request, $id)
     {
         $rep = $this->getDoctrine()->getRepository('AppBundle:Article');
-        $article = $rep->findOneBy( array('id' => $id));
-        return $this->render('show_article.html.twig', array('article' => $article));
+        $article = $rep->findOneBy(array('id' => $id));
+        return array('article' => $article);
     }
 }
