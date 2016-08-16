2af645c22073d590c4cc5ea54ccee84a08293f95
Fran Raknic <fran.raknic@gmail.com>
2016-08-16 16:04:54 +0200

    more attributes scraped and minor fixes

--- src/AppBundle/Command/JutarnjiCommand.php
+++ src/AppBundle/Command/JutarnjiCommand.php
@@ -6,19 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class indexCommand extends ContainerAwareCommand
+class JutarnjiCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
         $this
-            ->setName('scrape:index')
-            ->setDescription('Scrapes index.hr for articles.')
-            ->setHelp('Some text for --help user input');
+            ->setName('scrape:jutarnji')
+            ->setDescription('Scrapes jutarnji.hr for articles.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $scraper = $this->getContainer()->get('index-scraper');
-        $output->writeln('I am a scraper. I scrape all day and I scrape all night!');
+        $scraper = $this->getContainer()->get('jutarnji-scraper');
+        $scraper->fetchArticles();
+
     }
 }
