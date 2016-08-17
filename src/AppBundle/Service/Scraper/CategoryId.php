7977a9a2185bf2c1c1c33a01655bc760cade543d
Fran Raknic <fran.raknic@gmail.com>
2016-08-17 11:42:46 +0200

    removed public from const

--- src/AppBundle/Service/Scraper/CategoryId.php
+++ src/AppBundle/Service/Scraper/CategoryId.php
@@ -5,16 +5,16 @@ namespace AppBundle\Service\Scraper;
 
 class CategoryId
 {
-    public const hrvatska = 1;
-    public const regija = 2;
-    public const zagreb = 3;
-    public const svijet = 4;
-    public const crna_kronika = 5;
-    public const nogomet = 6;
-    public const kosarka = 7;
-    public const tenis = 8;
-    public const tvrtke = 9;
-    public const karijere = 10;
-    public const financije_i_trzista = 11;
+    const hrvatska = 1;
+    const regija = 2;
+    const zagreb = 3;
+    const svijet = 4;
+    const crna_kronika = 5;
+    const nogomet = 6;
+    const kosarka = 7;
+    const tenis = 8;
+    const tvrtke = 9;
+    const karijere = 10;
+    const financije_i_trzista = 11;
 
 }
