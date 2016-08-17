3a5fea8804ea2234cc210a8c8985165f93d62025
Marko Pađen <markopaden3@gmail.com>
2016-08-17 15:12:50 +0200

    added admin features

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AdminController extends Controller
 {
@@ -20,6 +22,7 @@ class AdminController extends Controller
 
         return array();
     }
+
     /**
      * @Route("/admin/option_1", name="option_1")
      * @Template()
@@ -28,8 +31,47 @@ class AdminController extends Controller
     {
         //Podešavanje vidljivosti pojedinačnih članaka
 
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Article');
+        $query = $repository->createQueryBuilder('a')
+            ->innerJoin('a.categories', 'c');
+        $articles = $query->getQuery()->getResult();
 
 
-        return array();
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData);
+        foreach ($articles as $article) {
+
+            if ($article->getVisible()) {
+
+
+                $form->add($article->getId(), CheckboxType::class, array('required' => false, 'data' => true, 'label' => $article->getTitle()));
+            } else {
+                $form->add($article->getId(), CheckboxType::class, array('required' => false, 'data' => false, 'label' => $article->getTitle()));
+            }
+        }
+        $form = $form->add('save', SubmitType::class, array('label' => 'Unesi'))->getForm();
+
+
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            foreach ($data as $id => $visible) {
+                if ($visible == false) {
+                    $visible = 0;
+                } else {
+                    $visible = 1;
+                }
+                $query = $em->createQuery("UPDATE AppBundle:Article a SET a.visible =" . $visible . " WHERE a.id=" . $id." AND a.visible!=".$visible);
+                $result = $query->getResult();
+            }
+
+            return $this->redirectToRoute('option_1');;
+        }
+
+
+        return array('articles' => $articles, 'form' => $form->createView());
     }
 }
