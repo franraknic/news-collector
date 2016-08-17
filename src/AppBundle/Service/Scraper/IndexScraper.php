5c4867ba6f1635d0fc20eb372d3db6a5c4334e50
Fran Raknic <fran.raknic@gmail.com>
2016-08-17 10:29:13 +0200

    minor fixes, again

--- src/AppBundle/Service/Scraper/IndexScraper.php
+++ src/AppBundle/Service/Scraper/IndexScraper.php
@@ -44,7 +44,7 @@ class IndexScraper extends BaseScraper
     /**
      * Returns array of articles from given URL
      * @param $articleUrls
-     * @param $i
+     * @param $id
      * @return array
      */
     protected function processUrls($articleUrls,$id)
